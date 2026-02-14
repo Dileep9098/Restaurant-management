ced46a3a46a5417bffa93c89f09bd88a84215300
Dileep Sahu <118627904+Dileep9098@users.noreply.github.com>
2026-02-14 15:33:59 +0530

    Update orderController.js

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1283,9 +1283,14 @@ export const downloadInvoice = async (req, res) => {
 `;
 
         // const browser = await puppeteer.launch();
-        const browser = await puppeteer.launch({
+   const browser = await puppeteer.launch({
     headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"]
+    args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--single-process"
+    ]
 });
         const page = await browser.newPage();
 
